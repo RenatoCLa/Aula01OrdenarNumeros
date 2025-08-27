839315e247df2289aa3f63fb346477faa3a8c5c3
Renato Costa <132628695+RenatoCLa@users.noreply.github.com>
2025-08-27 20:27:08 -0300

    Add files via upload

---
+++ ordernar.py
@@ -0,0 +1,26 @@
+desordenado = [15,2,6,6,6,9,7,10,1,24,12,12,15,6,7,8,7,6,9,20]
+resultado = []
+
+#Enquanto desordenado não estiver vazio
+while len(desordenado) > 0:
+  #cria uma variavel vazia chamada menor valor
+  menor_valor = None
+  #checa todos os numeros restantes
+  for x in desordenado:
+    #se não tiver um menor valor (ou seja, é o inicio de um novo loop) o primeiro numero será o menor valor
+    if menor_valor is None:
+      #menor valor é igual ao valor atual
+      menor_valor = x
+    #se a variavel menor valor não estiver vazia, compare ela com o numero atual
+    #se o numero atual for menor, substitua a variavel
+    elif menor_valor > x:
+      menor_valor = x
+  #remove o menor valor da lista de numeros
+  desordenado.remove(menor_valor)
+  #coloca esse valor na lista de numeros ordenada
+  resultado.append(menor_valor)
+  #limpa a variavel menor valor para ser utilizada no proximo número
+  menor_valor = None
+
+print(resultado)
+input()
